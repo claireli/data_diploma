9a15f2564e2a74759cf41e78d3b904e8c5bea2b9
Luis <lgmeraz@live.com>
2015-11-15 23:30:29 -0800

    cleanup

--- better.R
+++ better.R
@@ -41,3 +41,25 @@ num_census<-census[ , sapply(census, is.numeric)]
 weights <- information.gain(ALL_RATE_1112 ~. , num_census)
 weights <- information.gain(ALL_RATE_1112 ~. , num_grad)
 fit<-lm(num_grad$ALL_RATE_1112 ~ num_grad$MWH_RATE_1112 + num_grad$ECD_RATE_1112, data = num_grad)
+
+
+
+#GRADUATION_WITH_CENSUS_cleansed<-read.csv("GRADUATION_WITH_CENSUS_cleansed.csv")
+#g<-GRADUATION_WITH_CENSUS_cleansed[,c(3,5:25,40:581)]
+#g<-subset(g, select=-c(grep("MOE", colnames(g), perl=TRUE)))
+#COHORT_PCT<-function(COHORT, COHORT_TOTAL)
+#  COHORT_PCT<-COHORT/COHORT_TOTAL
+#return(COHORT_PCT)
+#MAM_PCT<-COHORT_PCT(g$MAM_COHORT_1112, g$ALL_COHORT_1112)
+#MAS_PCT<-COHORT_PCT(g$MAS_COHORT_1112, g$ALL_COHORT_1112)
+#MBL_PCT<-COHORT_PCT(g$MBL_COHORT_1112, g$ALL_COHORT_1112)
+#MHI_PCT<-COHORT_PCT(g$MHI_COHORT_1112, g$ALL_COHORT_1112)
+#MTR_PCT<-COHORT_PCT(g$MTR_COHORT_1112, g$ALL_COHORT_1112)
+#MWH_PCT<-COHORT_PCT(g$MWH_COHORT_1112, g$ALL_COHORT_1112)
+#CWD_PCT<-COHORT_PCT(g$CWD_COHORT_1112, g$ALL_COHORT_1112)
+#ECD_PCT<-COHORT_PCT(g$ECD_COHORT_1112, g$ALL_COHORT_1112)
+#LEP_PCT<-COHORT_PCT(g$LEP_COHORT_1112, g$ALL_COHORT_1112)
+#grad<-cbind(g, data.frame(cbind( MAM_PCT, MAS_PCT, MBL_PCT, MHI_PCT, MTR_PCT, MWH_PCT, CWD_PCT, ECD_PCT, LEP_PCT)))
+
+
+
